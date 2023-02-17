5beb9bdf5344aa9775606e1071a58028bec0a09e
Corbin Broadhead <poujmail@gmail.com>
2023-02-16 18:36:56 -0700

    Final

--- challenge-stock-prices.js
+++ challenge-stock-prices.js
@@ -1,60 +1,82 @@
-const best = (arr) => {
-    let max = 0;
-    let min = Math.min(...arr);
-    let arr2 = [];
-    for (let i = 0; i < arr.length; i++) {
-        // if (arr.indexOf(Math.min(...arr)) === arr.length - 1) {
-        //     arr.splice((arr.length - 1), 1)
-        //     console.log(arr);
-        // }
-        if (Math.min(...arr) === arr[arr.length - 1]) {
-            arr.splice((arr.length - 1), 1);
-            console.log(arr);
+const best = prices => {
+    let maxDiff = 0
+    let lowSoFar
+  
+    for (let i = 0; i < prices.length; i++) {
+        if (lowSoFar === undefined || prices[i] < lowSoFar) {
+            lowSoFar = prices[i]
         }
-        // if (arr.indexOf(Math.max(...arr)) === 0) {
-        //     arr.splice(0, 1);
-        //     console.log(arr);
-        // }
-        if (Math.max(...arr) === arr[0]) {
-            arr.splice(0, 1);
-            console.log(arr);
-        }
-    }   
-    for (i = 0; i < arr.length; i++) {
-        arr2.push(min);
-    }
-    for (i = 0; i < arr.length; i++) {
-        if (arr.indexOf(Math.max(...arr)) > arr.indexOf(Math.min(...arr))) {
-            let max = Math.max(...arr);
-            let min = Math.min(...arr);
-            let profit = max - min;
-            return `${profit} - buy at $${min}, sell at $${max}.`
-        } else if (arr.indexOf(Math.min(...arr)) > arr.indexOf(Math.max(...arr))) {
-            arr.splice(arr.indexOf(Math.min(...arr)), 1);
-        } else {
-            return 0;
+  
+        let diff = prices[i] - lowSoFar
+  
+        if (diff > maxDiff) {
+            maxDiff = diff
         }
     }
+  
+    return maxDiff
 }
-//     const motor = (arr) => {
-//         for (i = 0; i < arr.length; i++) {
+  
+best([1, 2, 3, 4, 5])\
+best([2, 3, 10, 6, 4, 8, 1])
+best([5, 4 , 3, 2, 1])
+// const best = (arr) => {
+//     let max = 0;
+//     let min = Math.min(...arr);
+//     let arr2 = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         // if (arr.indexOf(Math.min(...arr)) === arr.length - 1) {
+//         //     arr.splice((arr.length - 1), 1)
+//         //     console.log(arr);
+//         // }
+//         if (Math.min(...arr) === arr[arr.length - 1]) {
+//             arr.splice((arr.length - 1), 1);
+//             console.log(arr);
+//         }
+//         // if (arr.indexOf(Math.max(...arr)) === 0) {
+//         //     arr.splice(0, 1);
+//         //     console.log(arr);
+//         // }
+//         if (Math.max(...arr) === arr[0]) {
+//             arr.splice(0, 1);
+//             console.log(arr);
+//         }
+//     }   
+//     for (i = 0; i < arr.length; i++) {
+//         arr2.push(min);
+//     }
+//     for (i = 0; i < arr.length; i++) {
+//         if (arr.indexOf(Math.max(...arr)) > arr.indexOf(Math.min(...arr))) {
 //             let max = Math.max(...arr);
-//             if (Math.max(arr[i]) === 0) {
-//                 arr.splice(0, 1);
-//                 arr2.splice(0, 1);
-//                 motor(arr);
-//             }else if (Math.min(arr[i]) === arr.length - 1) {
-//                 arr.splice((arr.length - 1), 1);
-//             }else if (arr.length === 1) {
-//                 return 0;
-//             }else {
-//                 let profit = max - min;
-//                 return `${profit} - buy at $${min}, sell at $${max}.`
-//             }
+//             let min = Math.min(...arr);
+//             let profit = max - min;
+//             return `${profit} - buy at $${min}, sell at $${max}.`
+//         } else if (arr.indexOf(Math.min(...arr)) > arr.indexOf(Math.max(...arr))) {
+//             arr.splice(arr.indexOf(Math.min(...arr)), 1);
+//         } else {
+//             return 0;
 //         }
 //     }
-//     return motor(arr);
 // }
+// //     const motor = (arr) => {
+// //         for (i = 0; i < arr.length; i++) {
+// //             let max = Math.max(...arr);
+// //             if (Math.max(arr[i]) === 0) {
+// //                 arr.splice(0, 1);
+// //                 arr2.splice(0, 1);
+// //                 motor(arr);
+// //             }else if (Math.min(arr[i]) === arr.length - 1) {
+// //                 arr.splice((arr.length - 1), 1);
+// //             }else if (arr.length === 1) {
+// //                 return 0;
+// //             }else {
+// //                 let profit = max - min;
+// //                 return `${profit} - buy at $${min}, sell at $${max}.`
+// //             }
+// //         }
+// //     }
+// //     return motor(arr);
+// // }
 
-arr1 = [7, 9, 5, 6, 3, 2];
-console.log(best(arr1));
+// arr1 = [7, 9, 5, 6, 3, 2];
+// console.log(best(arr1));
